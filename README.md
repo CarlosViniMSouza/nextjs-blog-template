64e40ce9e21fdd5961c0d5557086656ecf73bb7d
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-04-10 22:24:09 -0300

    first step - ok

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
+![localhost](../First_App_NextJS/nextjs-blog/images/localhost.jpg/)
---
+++ autogit.sh
@@ -0,0 +1,7 @@
+git add .
+git commit -m "first step - ok"
+git branch -M main
+git remote add origin https://github.com/CarlosViniMSouza/nextjs-blog-template.git
+git push -u origin main
+git status 
+clear
